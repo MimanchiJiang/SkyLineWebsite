ef7be53d11bd67439cb6f9f042249943afc583f5
MiManchi <2237268069@qq.com>
2022-05-23 12:07:40 +0800

    deploy

---
+++ precache-manifest.9e51e3ad768127431057a3239322ee45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65357ac747e78f64fc3f",
+    "url": "/ SkyLineWebsite/css/app.7a6957c2.css"
+  },
+  {
+    "revision": "0a67195f7ce4d9921507",
+    "url": "/ SkyLineWebsite/css/chunk-vendors.71a233fb.css"
+  },
+  {
+    "revision": "eb59337aee947e8935a960b5b76ef54a",
+    "url": "/ SkyLineWebsite/index.html"
+  },
+  {
+    "revision": "65357ac747e78f64fc3f",
+    "url": "/ SkyLineWebsite/js/app.373d7a98.js"
+  },
+  {
+    "revision": "0a67195f7ce4d9921507",
+    "url": "/ SkyLineWebsite/js/chunk-vendors.bf4877cc.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/ SkyLineWebsite/manifest.json"
+  },
+  {
+    "revision": "08da34d92d3ba309261e177cda3e21a3",
+    "url": "/ SkyLineWebsite/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ SkyLineWebsite/robots.txt"
+  }
+]);
